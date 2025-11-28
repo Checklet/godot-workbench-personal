e34fcace6cddd2049ede676010bce4e546180f20
Checklet <ac.cekic@outlook.com>
2025-11-28 02:07:29 +0100

    Refactored: Calculating direction

--- scenes/character/Zoroark.cs
+++ scenes/character/Zoroark.cs
@@ -19,79 +19,71 @@ public partial class Zoroark : AnimatedSprite2D
         if (Input.IsKeyPressed(Key.S)) currentInput += 0b0100;
         if (Input.IsKeyPressed(Key.A)) currentInput += 0b1000;
 
-        if (currentInput != _lastInput)
-        {
-            switch (currentInput)
-            {
-                case 0b0000:
-                    // idle
-                    PlayIdle();
-                    break;
-
-                case 0b0001:
-                case 0b1011:
-                    // walk up
-                    Direction = 0b0001;
-                    PlayWalk();
-                    break;
-
-                case 0b0010:
-                case 0b0111:
-                    // walk right
-                    Direction = 0b0010;
-                    PlayWalk();
-                    break;
-
-                case 0b0100:
-                case 0b1110:
-                    // walk down
-                    Direction = 0b0100;
-                    PlayWalk();
-                    break;
-
-                case 0b1000:
-                case 0b1101:
-                    // walk left
-                    Direction = 0b1000;
-                    PlayWalk();
-                    break;
-
-                case 0b0101:
-                case 0b1010:
-                case 0b1111:
-                    // no valid input; spin
-                    PlayIdle();
-                    break;
-
-                case 0b0011:
-                case 0b0110:
-                case 0b1100:
-                case 0b1001:
-                    // no valid input; calculate due to last given input
-                    switch (Direction)
-                    {
-                        case 0b0001:
-                        case 0b0010:
-                        case 0b0100:
-                        case 0b1000:
-                            Direction ^= currentInput;
-                            PlayWalk();
-                            break;
-
-                        default:
-                            break;
-                    }
-                    break;
-
-                default:
-                    currentInput = 0;
-                    break;
-            }
-
-            _lastInput = currentInput;
-        }
-
+        if (currentInput != _lastInput) UpdateDirection(_lastInput = currentInput);
         if (_isWalking) Position += GetDirectionVector() * 3;
+    }
+
+    private void UpdateDirection(byte input)
+    {
+        switch (input)
+        {
+            case 0b0000:
+                // idle
+                PlayIdle();
+                break;
+
+            case 0b0001:
+            case 0b1011:
+                // walk up
+                Direction = 0b0001;
+                PlayWalk();
+                break;
+
+            case 0b0010:
+            case 0b0111:
+                // walk right
+                Direction = 0b0010;
+                PlayWalk();
+                break;
+
+            case 0b0100:
+            case 0b1110:
+                // walk down
+                Direction = 0b0100;
+                PlayWalk();
+                break;
+
+            case 0b1000:
+            case 0b1101:
+                // walk left
+                Direction = 0b1000;
+                PlayWalk();
+                break;
+
+            case 0b0101:
+            case 0b1010:
+            case 0b1111:
+                // no valid input; spin
+                PlayIdle();
+                break;
+
+            case 0b0011:
+            case 0b0110:
+            case 0b1100:
+            case 0b1001:
+                // no valid input; calculate due to last given input
+                switch (Direction)
+                {
+                    case 0b0001:
+                    case 0b0010:
+                    case 0b0100:
+                    case 0b1000:
+                        Direction ^= input;
+                        PlayWalk();
+                        break;
+                }
+                break;
+        }
     }
 
     private void PlayWalk()
